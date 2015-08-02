9bf5b16f1202270ac538fb34f251e867065d4d7f
Thapelo Sebolai <thapz.sebolai@gmail.com>
2015-08-02 22:07:09 +0200

    Added 'remove' functionality

---
+++ Resolutions.js
@@ -0,0 +1,51 @@
+Resolution = new Mongo.Collection("resolutions");
+
+if (Meteor.isClient) {
+    Template.body.helpers({
+        resolutions: function () {
+            if(Session.get("hideFinished")){
+                return Resolution.find({checked:{$ne: true }})
+
+            }else{
+                return Resolution.find();
+            }
+            
+        },
+        hideFinished:function(){
+            return Session.get("hideFinished");
+
+        }
+    });
+    Template.body.events({
+        "submit .new-resolution": function (event) {
+            var title = event.target.title.value;
+            Resolution.insert({
+                title: title,
+                createdAt: new Date()
+            });
+            event.target.title.value = "";
+            return false;
+        },
+        "change .hide-finished":function(event){
+            Session.set("hideFinished", event.target.checked)
+        }
+    });
+    Template.resolution.events({
+        "click .toggle-checked": function () {
+            Resolution.update(this._id, {
+                $set: {
+                    checked: !this.checked
+                }
+            });
+        },
+        "click .delete": function () {
+            Resolution.remove(this._id);
+        }
+    });
+}
+
+if (Meteor.isServer) {
+    Meteor.startup(function () {
+        // code to run on server at startup
+    });
+}
